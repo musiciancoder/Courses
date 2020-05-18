47105d13b6e2ccd7cc1344dcf1a2c14d9bad6545
musiciancoder <andresguitarra1980@gmail.com>
2020-05-18 16:58:19 -0400

    Spring security - stack en problema con los tipos de las autorizaciones en UserController

---
+++ src/main/java/com/example/courses/entity/User.java
@@ -0,0 +1,40 @@
+package com.example.courses.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String username;
+	private String pwd;
+	private String token;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+}
