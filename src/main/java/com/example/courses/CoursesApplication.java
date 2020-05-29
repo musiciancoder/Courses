bd106366ce7083e967bca43020250689bbd95c46
musiciancoder <andresguitarra1980@gmail.com>
2020-05-28 20:41:35 -0400

    deploy test 2, according to medium.com

--- src/main/java/com/example/courses/CoursesApplication.java
+++ src/main/java/com/example/courses/CoursesApplication.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
@@ -13,8 +15,13 @@ import com.example.courses.entity.Student;
 import com.example.courses.repository.IStudentRepository;
 
 @SpringBootApplication
-public class CoursesApplication implements CommandLineRunner { 
+public class CoursesApplication extends SpringBootServletInitializer implements CommandLineRunner { 
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CoursesApplication.class);
+    }
+	
 	@Autowired
 	private IStudentRepository repoEstudiante;
 	
